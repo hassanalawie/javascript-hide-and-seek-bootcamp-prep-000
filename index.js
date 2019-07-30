b6ca7c33ff98e45460da1eaca922de41d5a55c08
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-30 20:09:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  let temp = document.querySelector('#grand-node')
+  let nextNode = temp.children[0]
+
+  while (nextNode) {
+    temp = nextNode
+    nextNode = temp.children[0]
+  }
+
+  return temp
+  }
